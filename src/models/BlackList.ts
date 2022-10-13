7a6de32f9d9ff92fd89d3dc0d59208db88cc5c07
Michal <michal.blaszkiewicz@britenet.com.pl>
2022-10-13 08:57:30 +0200

    Fix

--- src/models/BlackList.ts
+++ src/models/BlackList.ts
@@ -3,7 +3,7 @@ import { TBlackList } from '../types/types';
 
 const BlackListSchema: Schema<TBlackList> = new Schema ({
     token: { type: String, required: true, unique: true },
-    expireAt: { type: Date, default: new Date(), expires: 600 },
+    expireAt: { type: Date, default: new Date(), expires: 600, required: true },
 });
 
 export default mongoose.model('BlackList', BlackListSchema);
